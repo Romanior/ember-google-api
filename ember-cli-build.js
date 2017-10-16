d046963fd3965bf5851459faa7a7b4468934bc85
Roman <roma.khudyakov@gmail.com>
2017-10-16 15:50:29 +0300

    add deployment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,20 @@
 /* eslint-env node */
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+var env = EmberApp.env() || 'development';
+var isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1;
+var fingerprint = {
+  enabled: isProductionLikeBuild,
+  prepend: (env === 'production') ? '' : 'http://app.ember-google-api.com.s3-website.eu-central-1.amazonaws.com/',
+  extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg', 'pdf']
+}
+
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    fingerprint
   });
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
